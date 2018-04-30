ac167235c1761657a755aad8be3968cdc581fce4
Adriana Shulman <adriana33@mac.com>
2018-04-29 23:24:29 -0400

    add game fuctionality

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+
+
+// $(document).ready()
+// //Create variables 
+// var minNumber = 20;
+// var maxNumber = 60;
+// var wins;
+// var loses;
+
+// var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+
+// function randomNumberFromRange(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// console.log(randomNumber);
